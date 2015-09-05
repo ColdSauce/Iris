e4841c674545892dfc6a8390574cec7c2836e004
jmiller656 <jxm7374@rit.edu>
2015-09-05 06:48:42 -0400

    Add code to accomodate a new '3 circles' approach

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from SimpleCV import *
 winsize = (640,480)
 display = Display(winsize)
-
 video = VirtualCamera('stefan_eye.mp4', 'video')
 while display.isNotDone():
-    a = video.getImage()
-    a.rotate(90).invert().toGray().binarize().save(display)
-
+    image = video.getImage().rotate(90).crop(850,50,400,400)
+    image2 = image.colorDistance(Color.RED)
+    blobs = image2.findBlobs()
+    image3 = image2.grayscale()
+    if blobs:
+        for b in blobs:
+            if b.isCircle(0.7) and b.radius() > 3:
+                image.drawCircle((b.x,b.y),b.radius(),Color.YELLOW,2)
+    image.show()
